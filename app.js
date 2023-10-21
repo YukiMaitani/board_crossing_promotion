57c538a70bc61113e18e28d7b6688defb0ee93a7
YukiMaitani <fightzoi1997@gmail.com>
2023-10-21 19:08:12 +0900

    動くモデルを追加

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 let model;
+let mixer;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -15,13 +16,24 @@ const renderer = new THREE.WebGLRenderer({ antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
 const controls = new OrbitControls( camera, renderer.domElement );
 
 const loader = new GLTFLoader();
-loader.load( 'models/scene.gltf', function ( gltf ) {
+loader.load( 'models/cheesecake/scene.gltf', function ( gltf ) {
   model = gltf.scene;
-  model.position.set(0, 0, 0);
+  model.position.set(0, -0.5, 0);
+  model.scale.set(0.0001, 0.0001, 0.0001);
   scene.add( model );
+
+  mixer = new THREE.AnimationMixer( model );
+  const clips = gltf.animations;
+  clips.forEach(clip => {
+    const action = mixer.clipAction( clip );
+    action.play();
+  });
 }, undefined, function ( error ) {
   console.error( error );
 } );
@@ -31,6 +43,10 @@ camera.position.set(0, 0, 1);
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
+
+  if (mixer) {
+    mixer.update(0.01);
+  }
 
   renderer.render(scene, camera);
 }
